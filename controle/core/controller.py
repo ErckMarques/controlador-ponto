3e24f3f190713ff7e2979cb6b10c474fa46b2238
Erik Marques <lucroalternativo@outlook.com>
2025-02-14 13:16:56 -0300

    Módulo do objeto que gerencia o fluxo de trabalho da aplicação. O coração da app.

---
+++ controle/core/controller.py
@@ -0,0 +1,11 @@
+import argparse
+
+from datetime import datetime, date
+from typing import Literal, Union, Optional
+
+CARIMBO = Literal['']
+
+class ControllerHour:
+
+    def __init__(self, carimbo):
+        pass
