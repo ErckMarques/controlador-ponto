fb7063974b13da6a8b9ab48db4eddd773924c0b5
Erik Marques <lucro.alternativo@outlook.com>
2025-02-16 17:13:27 -0300

    Definie a Aplication Factory create_parser onde instaciamos e configuramos o parser da CLI._

--- controle/cli/__init__.py
+++ controle/cli/__init__.py
@@ -1,3 +1,24 @@
 """
-Este módulo contém a classe principal do fluxo de trabalho.
+Este módulo realiza a configuração do pacote `cli` onde encontra-se o parser da linha de comando.
 """
+from argparse import ArgumentParser
+
+from controle.cli import config
+
+def create_parser() -> ArgumentParser:
+    """
+    Cria um parser de linha de comando para o pacote `cli`.
+
+    Returns:
+        ArgumentParser: O parser de linha de comando configurado.
+    """
+    parser = ArgumentParser(
+        prog='HControll',
+        description='%(prog)s - Programa de linha de comando para controle pessoal de horas trabalhadas.',
+        conflict_handler='resolve',
+    )
+
+    # configurando as ocções do parser
+    config.init_parser(parser)
+
+    return parser
