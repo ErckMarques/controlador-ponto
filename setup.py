a71bb30904aa695ec2f649e75c0fc399327877a1
Erik Marques <lucro.alternativo@outlook.com>
2025-02-17 01:20:33 -0300

    Adiciodos novos metadados e atualizados os antigos.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
+
 import subprocess
 
 class BuildWithDocs(build_py):
@@ -10,7 +11,31 @@ class BuildWithDocs(build_py):
         super().run()
 
 setup(
+    name='HControll',
+    version='0.2.0',
+    description='Um projeto de controle de horas para uso pessoal.',
+    author='Erik Marques',
+    author_email='lucro.alternativo@outlook.com',
+    url='https://github.com/ErckMarques/controlador-ponto.git',
+    maintainer='Erik Marques',
+    packages=find_packages(),
+    python_requires='>=3.9',
+    install_requires=[
+        'dateparser',
+    ],
+    extras_require={
+        'dev': ['taskipy'],
+        'test': ['pytest', 'pytest-cov', 'coverage'],
+        'doc': ['mkdocs', 'mkdocstrings[python]', 'mkdocs-material'],
+    },
+    include_package_data=True,
+    zip_safe=False,
     cmdclass={
-        "build_py": BuildWithDocs,
+        'build_py': BuildWithDocs,
+    },
+    entry_points={
+        'console_scripts': [
+            'HControll = controle.__main__:main',
+        ],
     },
 )
