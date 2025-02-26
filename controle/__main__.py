4bad736c7bb417378f1eb31c856aa22cd54c11fa
Erik Marques <lucro.alternativo@outlook.com>
2025-02-25 23:14:49 -0300

    Add novos fluxos na função main

--- controle/__main__.py
+++ controle/__main__.py
@@ -1,13 +1,37 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from typing import List, Literal, Tuple
 
 from controle.cli import create_parser
 from controle.core.model import RecordHour
+from controle.cli import tratar_horarios
 
 def main():
     # instancia o parser
     parser = create_parser()
-    # le os argumentos
+    # lê os argumentos
     args = parser.parse_args()
+
+    # verifica se foi passado um caminho de arquivo
+    if args.file is not None:
+        # Abre o arquivo 
+        with args.file.open(mode='r', encoding='UTF-8') as f:
+            # lê seu conteudo
+            lines = f.readlines()
+            # tratar o conteudo no formato d/m/Y; H:M -> H:M
+            lines = [line.strip('\n') for line in lines] #remove quebra de linha
+            horarios = tratar_horarios(lines)
+        
+        # passar os dados para RecordHour
+        exit()
+    
+    # verifica se foi passado algum horario manualmente
+    if args.horarios is not None:
+        # trata estes horarios
+        horarios = tratar_horarios(args.horarios)
+
+        # passa os dados para RecordHour
+        exit()
+        
     # monto os dados
     inicio = datetime.combine(args.data, args.hora_inicio)
     final = datetime.combine(args.data, args.hora_final)
